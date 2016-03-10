606309d9c79949f4e7750f7453f7bd1c1bb610ce
kaos34k <kaos34@gmail.com>
2016-03-10 09:58:24 -0500

    por favor revisar
    hacer las correcciones pertinentes para aceptar los cambios

--- ProcedimientosAlamcenadosTransaciones.sql
+++ ProcedimientosAlamcenadosTransaciones.sql
@@ -15,14 +15,36 @@ AS BEGIN
         END
       ELSE IF ( @tipo = servicio )
         BEGINT
+          IF ( @id = NULL )
+            BEGINT
+              @ultimo_registro SELECT MAX(id) FROM producto;
+              @id =  @ultimo_registro + 1;
+            END
+          END
           @valor_iva =  @valor_neto * 0.5;
           INSERT INTO producto  (id, nombre, valor_neto, tipo)
                  VALUES (@id, @nombre, @valor_neto, @tipo);
           SELECT "El prodcto ya a sido creado y se le a incrementado un 0.5% al valor real" AS mensaje;       
         END
       ELSE IF ( @tipo = bien )
+          @valor_iva =  @valor_neto * 0.2;
+          INSERT INTO producto  (id, nombre, valor_neto, tipo)
+                 VALUES (@id, @nombre, @valor_neto, @tipo);
+          SELECT "El prodcto ya a sido creado y se le a incrementado un 0.5% al valor real" AS mensaje;    
+      END
+      IF(@@ERROR)
+        BEGINT
+          ROLLBACK guardar
+          SELECT "Existe un error por favor vielve a intentarlo de nuevo" AS mensaje;
+        END
+      ELSE
+        BEGINT
+          COMMIT guardar
+        END
       END
     END TRY
     BEGIN CATCH
+      ERROR_MESSAGE() AS mensaje;
     END CATCH
   END
+EXECUTE crear_producto @id = NULL,  @nombre = "pañales"  @valor_neto  = 12312,@valor_iva,@tipo = "servicio";

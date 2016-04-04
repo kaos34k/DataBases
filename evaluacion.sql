8315882ddaa9c3277358a67e0a1bfe2b4cc1269f
kaos34k <kaos34@gmail.com>
2016-04-03 19:28:41 -0500

    s

--- evaluacion.sql
+++ evaluacion.sql
@@ -2,7 +2,27 @@ CREATE DATABASE evaluacion
 GO
 USE evaluacion
 GO
-CREATE TABLE (
-
+CREATE TABLE modelo (
+  id_modelo int primary key,
+)
+GO
+CREATE TABLE aereolinea(
+  id_aereolinea int primary key,
+)
+GO
+CREATE TABLE avion( 
+  id_avion int primary key,
+)
+GO
+CREATE TABLE pais(
+  id_pais int primary key,
+)
+GO
+CREATE TABLE aereopuerto(
+  id_aereopuerto int primary key,
+)
+GO
+CREATE TABLE escala(
+  id_escala int primary key,
 )
 GO

1c458f9d6bdd6cba0b08b618b49b1704c8231186
kaos34k <kaos34@gmail.com>
2016-05-25 13:45:40 -0500

    cración de tablas

---
+++ wallet.sql
@@ -0,0 +1,18 @@
+create database wallet
+go 
+use wallet 
+go 
+create table rol(
+)
+go
+create table tipoCuenta(
+)
+go
+create table usuario(
+)
+go 
+create table tipoMovimiento(
+)
+go 
+create table estadoCuenta(
+)
